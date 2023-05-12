d57211df7478efb3c50271379a7d121c6db6942b
moonhee <zipsy327@gmail.com>
2023-05-12 12:29:13 +0900

    jenkins-test

---
+++ src/main/java/bit/study/main/CloudAiServiceApplication.java
@@ -0,0 +1,13 @@
+package bit.study.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bit/study/main/CloudAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.study.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
